8a6b29f753a6c0c09f03302ff22272e82343f7f5
fuyutarow <fuyutarow@gmail.com>
2021-05-15 17:46:13 +0900

    Fix crate::Config.write_nu

--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -25,47 +25,56 @@ pub struct Config {
 }
 
 impl Config {
-    fn to_nu(&self) -> anyhow::Result<String> {
-        let mut s = String::new();
-        writeln!(s, "startup = [");
+    fn to_nu(&self) -> anyhow::Result<Vec<String>> {
+        let mut lines = Vec::<String>::new();
+        lines.push(String::from("startup = ["));
+
         for alias in self.clone().aliases {
-            writeln!(s, "    {},", alias.to_nu());
+            lines.push(format!("    {},", alias.to_nu()));
         }
-        writeln!(s, "]");
-        writeln!(
-            s,
+        lines.push(String::from("]"));
+
+        lines.push(format!(
             r##"
 path = [
-    "/home/fuyutarow/.linuxbrew/bin",
-    "/home/fuyutarow/.linuxbrew/sbin",
+"{home_dir}/.linuxbrew/bin",
+"{home_dir}/.linuxbrew/sbin",
 ]
 
 [env]
-HOMEBREW_PREFIX = "/home/fuyutarow/.linuxbrew"
-HOMEBREW_CELLAR = "/home/fuyutarow/.linuxbrew/Cellar"
-HOMEBREW_REPOSITORY = "/home/fuyutarow/.linuxbrew/Homebrew"
-# PATH = "/home/fuyutarow/.linuxbrew/bin:/home/fuyutarow/.linuxbrew/sbin${{PATH+:$PATH}}"
-MANPATH = "/home/fuyutarow/.linuxbrew/share/man${{MANPATH+:$MANPATH}}:"
-INFOPATH = "/home/fuyutarow/.linuxbrew/share/info:${{INFOPATH:-}}"
-        "##
-        );
-        Ok(s)
+HOMEBREW_PREFIX = "{home_dir}/.linuxbrew"
+HOMEBREW_CELLAR = "{home_dir}/.linuxbrew/Cellar"
+HOMEBREW_REPOSITORY = "{home_dir}/.linuxbrew/Homebrew"
+# PATH = {home_dir}/.linuxbrew/bin:{home_dir}/.linuxbrew/sbin${{PATH+:$PATH}}"
+MANPATH = "{home_dir}/.linuxbrew/share/man${{MANPATH+:$MANPATH}}:"
+INFOPATH = "{home_dir}/.linuxbrew/share/info:${{INFOPATH:-}}"
+"##,
+            home_dir = dirs::home_dir()
+                .unwrap()
+                .into_os_string()
+                .into_string()
+                .unwrap()
+        ));
+        Ok(lines)
     }
 
     pub fn print_nu(&self) {
-        if let Ok(s) = self.to_nu() {
-            println!("{}", s);
+        if let Ok(lines) = self.to_nu() {
+            for line in lines {
+                println!("{}", line);
+            }
         }
     }
 
-    pub fn write_nu(&self) -> anyhow::Result<String> {
-        let s = self.to_nu()?;
+    pub fn write_nu(&self) -> anyhow::Result<std::path::PathBuf> {
+        let lines = self.to_nu()?;
         let fpath = dirs::config_dir()
             .expect("expected file path")
             .join("nu/config.toml");
-        let mut f = std::fs::File::create(&fpath).expect("failed to create file");
-        f.write_all(&s.as_bytes()).expect("failed to write file");
-        Ok(s)
+
+        let s = lines.join("\n");
+        std::fs::write(&fpath, s)?;
+        Ok(fpath)
     }
 }
 
