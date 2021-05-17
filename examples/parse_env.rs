d4d9566370e3d1579dcec94f9fe5e7cc47c4d7b1
fuyutarow <fuyutarow@gmail.com>
2021-05-18 02:08:49 +0900

    Add crate::RawConfig

---
+++ examples/parse_env.rs
@@ -0,0 +1,32 @@
+use std::collections::HashMap;
+use std::io::prelude::*;
+use std::path::PathBuf;
+
+use serde_derive::{Deserialize, Serialize};
+
+use cli::Config;
+use cli::NuConfig;
+use cli::RawConfig;
+
+fn main() {
+    let fpath = "examples/env.toml";
+    let content = std::fs::read_to_string(fpath).expect("Unable to read file");
+    let raw = toml::from_str::<RawConfig>(&content).expect("Failed to parse as toml");
+    let config = Config::from(raw);
+
+    let nu = NuConfig::from(config);
+    nu.write();
+
+    // dbg!(nu);
+
+    // dbg!(&config);
+
+    // match toml::to_string(&config) {
+    //     Ok(toml) => {
+    //         println!("{}", toml);
+    //     }
+    //     Err(err) => {
+    //         eprintln!("{}", err)
+    //     }
+    // };
+}
--- src/lib/mod.rs
+++ src/lib/mod.rs
@@ -4,25 +4,16 @@ use std::fmt::Write as FmtWrite;
 use std::io::Write as IoWrite;
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
-pub struct Alias {
-    pub name: String,
-    pub body: String,
-}
-
-impl Alias {
-    pub fn to_bash(&self) -> String {
-        format!(r#"alias {} = "{}""#, self.name, self.body)
-    }
-
-    pub fn to_nu(&self) -> String {
-        format!(r#""alias {} = {}""#, self.name, self.body)
-    }
+pub struct RawConfig {
+    pub path: Option<Vec<String>>,
+    pub alias: Option<HashMap<String, String>>,
+    pub env: Option<HashMap<String, String>>,
 }
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Config {
     pub path: Vec<String>,
-    pub aliases: Vec<Alias>,
+    pub alias: HashMap<String, String>,
     pub env: HashMap<String, String>,
 }
 
@@ -33,12 +24,22 @@ pub struct NuConfig {
     pub env: HashMap<String, String>,
 }
 
+impl From<RawConfig> for Config {
+    fn from(raw: RawConfig) -> Self {
+        Self {
+            path: raw.path.unwrap_or(Vec::new()),
+            alias: raw.alias.unwrap_or(HashMap::new()),
+            env: raw.env.unwrap_or(HashMap::new()),
+        }
+    }
+}
+
 impl From<Config> for NuConfig {
     fn from(config: Config) -> Self {
         let startup = config
-            .aliases
+            .alias
             .into_iter()
-            .map(|alias| format!("alias {} = {}", alias.name, alias.body))
+            .map(|(k, v)| format!("alias {} = {}", k, v))
             .collect::<Vec<String>>();
 
         Self {
@@ -49,56 +50,43 @@ impl From<Config> for NuConfig {
     }
 }
 
-impl Config {
-    fn to_nu(&self) -> anyhow::Result<Vec<String>> {
-        let mut lines = Vec::<String>::new();
-        lines.push(String::from("startup = ["));
-
-        for alias in self.clone().aliases {
-            lines.push(format!("    {},", alias.to_nu()));
-        }
-        lines.push(String::from("]"));
-
-        // "{home_dir}/.linuxbrew/bin",
-        // "{home_dir}/.linuxbrew/sbin",
-
-        lines.push(format!(
-            r##"
-path = [
-"$HOMEBREW_PREFIX/bin",
-"$HOMEBREW_PREFIX/sbin",
-]
-"##,
-            // home_dir = dirs::home_dir()
-            //     .unwrap()
-            //     .into_os_string()
-            //     .into_string()
-            //     .unwrap()
-        ));
-        Ok(lines)
-    }
-
-    pub fn print_nu(&self) {
-        if let Ok(lines) = self.to_nu() {
-            for line in lines {
-                println!("{}", line);
+impl NuConfig {
+    pub fn print(&self) {
+        match toml::to_string(&self) {
+            Ok(toml) => {
+                println!("{}", toml);
             }
-        }
+            Err(err) => {
+                eprintln!("{}", err)
+            }
+        };
     }
 
-    pub fn write_nu(&self) -> anyhow::Result<std::path::PathBuf> {
-        let lines = self.to_nu()?;
-        let fpath = dirs::config_dir()
+    pub fn write(&self) {
+        let nu_config_fpath = dirs::config_dir()
             .expect("expected file path")
             .join("nu/config.toml");
-
-        let s = lines.join("\n");
-        std::fs::write(&fpath, s)?;
-        Ok(fpath)
+        if let Ok(toml) = toml::to_string(&self) {
+            match std::fs::write(&nu_config_fpath, toml) {
+                Ok(_) => println!(
+                    "{} was updated",
+                    nu_config_fpath.into_os_string().into_string().unwrap()
+                ),
+                Err(err) => eprintln!("{}", err),
+            }
+        };
     }
 }
 
-#[derive(Debug, Clone, Deserialize, Serialize)]
-pub struct RawConfig {
-    alias: HashMap<String, String>,
+impl Config {
+    // pub fn write_nu(&self) -> anyhow::Result<std::path::PathBuf> {
+    //     let lines = self.to_nu()?;
+    //     let fpath = dirs::config_dir()
+    //         .expect("expected file path")
+    //         .join("nu/config.toml");
+
+    //     let s = lines.join("\n");
+    //     std::fs::write(&fpath, s)?;
+    //     Ok(fpath)
+    // }
 }
